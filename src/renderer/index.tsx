8aab69fe542ac42397f848bb85b03d7198b4610f
Talevv <talevv@proton.me>
2026-03-26 23:02:01 +0100

    Clean log

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/components/App';
-console.log('dwqdqw')
+
 const root = createRoot(document.body);
 root.render(<App />);
